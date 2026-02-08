ca8a433399a7b956e3204fff2ec02f4e439a8484
sts151718 <tsubokurajob151718@gmail.com>
2026-02-08 13:11:53 +0900

    タイトルを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import { Center, Heading } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Center w="100%" py="8">
+      <Heading as="h1" size="2xl" mb="4">
+        学習記録一覧
+      </Heading>
+    </Center>
   );
 }
 
